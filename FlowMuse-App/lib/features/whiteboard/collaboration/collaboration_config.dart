f7838abb54a41d60bad27e89589edaa3302b3358
Enchograph <Ender0213@outlook.com>
2026-07-07 13:35:00 +0800

    使用 dotenv 配置协作服务地址

--- FlowMuse-App/lib/features/whiteboard/collaboration/collaboration_config.dart
+++ FlowMuse-App/lib/features/whiteboard/collaboration/collaboration_config.dart
@@ -1,12 +1,19 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class CollaborationConfig {
   const CollaborationConfig({required this.serverUrl});
 
-  static const fromEnvironment = CollaborationConfig(
-    serverUrl: String.fromEnvironment(
+  static CollaborationConfig get fromEnvironment {
+    const dartDefinedServerUrl = String.fromEnvironment(
       'FLOWMUSE_COLLAB_SERVER_URL',
-      defaultValue: 'http://127.0.0.1:3000',
-    ),
-  );
+    );
+    final dotenvServerUrl = dotenv.maybeGet('FLOWMUSE_COLLAB_SERVER_URL');
+    return CollaborationConfig(
+      serverUrl: dartDefinedServerUrl.isNotEmpty
+          ? dartDefinedServerUrl
+          : dotenvServerUrl ?? 'http://127.0.0.1:3000',
+    );
+  }
 
   final String serverUrl;
 }
